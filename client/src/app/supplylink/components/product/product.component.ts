ef76913e324734eca22a447685e41735bf6015a8
ReddyJaiSaiPreetham <dummy@gmail.com>
2026-03-14 18:24:55 +0000

    day21

--- client/src/app/supplylink/components/product/product.component.ts
+++ client/src/app/supplylink/components/product/product.component.ts
@@ -1,4 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
-export class ProductComponent {
- 
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss']
+})
+export class ProductComponent implements OnInit {
+  productForm!: FormGroup;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    // Initializing the FormGroup with the specified validation rules
+    this.productForm = new FormGroup({
+      productId: new FormControl(''), // Optional
+      warehouseId: new FormControl('', [
+        Validators.required, 
+        Validators.min(1) // Must be greater than 0
+      ]),
+      productName: new FormControl('', [
+        Validators.required
+      ]),
+      productDescription: new FormControl(''),
+      quantity: new FormControl('', [
+        Validators.required, 
+        Validators.min(0) // Must be non-negative
+      ]),
+      price: new FormControl('', [
+        Validators.required, 
+        Validators.min(1) // Must be greater than 0
+      ])
+    });
+  }
+
+  onSubmit(): void {
+    if (this.productForm.valid) {
+      const productInstance = this.productForm.value;
+      console.log('Product Instance Created:', productInstance);
+      // Logic for further processing (e.g., API call) goes here
+    }
+  }
 }
--- client/src/app/supplylink/services/supplylink.service.ts
+++ client/src/app/supplylink/services/supplylink.service.ts
@@ -1,102 +1,85 @@
-import { Injectable } from "@angular/core";
+// import { Injectable } from "@angular/core";
 // import { environment } from "src/environments/environment";
-import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs";
-import { Supplier } from "../types/Supplier";
-import { Warehouse } from "../types/Warehouse";
-import { Product } from "../types/Product";
+// import { HttpClient } from "@angular/common/http";
+// import { Observable } from "rxjs";
+// import { Supplier } from "../types/Supplier";
+// import { Warehouse } from "../types/Warehouse";
+// import { Product } from "../types/Product";
 
-@Injectable({
-  providedIn: "root",
-})
-export class SupplyLinkService {
-  // private baseUrl = `${environment.apiUrl}`;
+// @Injectable({
+//   providedIn: "root",
+// })
+// export class SupplyLinkService {
+//   private baseUrl = `${environment.apiUrl}`;
 
-  constructor(private http: HttpClient) { }
+//   constructor(private http: HttpClient) { }
 
-  addSupplier(supplier: Supplier): Observable<any> {
-    // Implementation goes here
-    return new Observable<any>();
-  }
+//   addSupplier(supplier: Supplier): Observable<any> {
+//     return this.http.post<any>(`${this.baseUrl}/supplier`, supplier);
+//   }
 
-  editSupplier(supplier: Supplier): Observable<any> {
-    // Implementation goes here
-    return new Observable<any>();
-  }
+//   editSupplier(supplier: Supplier): Observable<any> {
+//     return this.http.put<any>(`${this.baseUrl}/supplier/${supplier.supplierId}`, supplier);
+//   }
 
-  deleteSupplier(supplierId: number): Observable<any> {
-    // Implementation goes here
-    return new Observable<any>();
-  }
+//   deleteSupplier(supplierId: number): Observable<any> {
+//     return this.http.delete(`${this.baseUrl}/supplier/${supplierId}`);
+//   }
 
-  getSupplierById(supplierId: number): Observable<Supplier> {
-    // Implementation goes here
-    return new Observable<Supplier>();
-  }
+//   getSupplierById(supplierId: number): Observable<Supplier> {
+//     return this.http.get<Supplier>(`${this.baseUrl}/supplier/${supplierId}`);
+//   }
 
-  getAllSuppliers(): Observable<Supplier[]> {
-    // Implementation goes here
-    return new Observable<Supplier[]>();
-  }
+//   getAllSuppliers(): Observable<Supplier[]> {
+//     return this.http.get<Supplier[]>(`${this.baseUrl}/supplier`);
+//   }
 
-  addWarehouse(warehouse: Warehouse): Observable<any> {
-    // Implementation goes here
-    return new Observable<any>();
-  }
+//   addWarehouse(warehouse: Warehouse): Observable<any> {
+//     return this.http.post<Warehouse>(`${this.baseUrl}/warehouse`, warehouse);
+//   }
 
-  editWarehouse(warehouse: Warehouse): Observable<any> {
-    // Implementation goes here
-    return new Observable<any>();
-  }
+//   editWarehouse(warehouse: Warehouse): Observable<any> {
+//     return this.http.put<any>(`${this.baseUrl}/warehouse/${warehouse.warehouseId}`, warehouse);
+//   }
 
-  deleteWarehouse(warehouseId: number): Observable<any> {
-    // Implementation goes here
-    return new Observable<any>();
-  }
+//   deleteWarehouse(warehouseId: number): Observable<any> {
+//     return this.http.delete(`${this.baseUrl}/warehouse/${warehouseId}`);
+//   }
 
-  getWarehouseById(warehouseId: number): Observable<Warehouse> {
-    // Implementation goes here
-    return new Observable<Warehouse>();
-  }
+//   getWarehouseById(warehouseId: number): Observable<Warehouse> {
+//     return this.http.get<Warehouse>(`${this.baseUrl}/warehouse/${warehouseId}`);
+//   }
 
-  getAllWarehouses(): Observable<Warehouse[]> {
-    // Implementation goes here
-    return new Observable<Warehouse[]>();
-  }
+//   getAllWarehouses(): Observable<Warehouse[]> {
+//     return this.http.get<Warehouse[]>(`${this.baseUrl}/warehouse`);
+//   }
 
-  getWarehousesBySupplier(supplierId: number | null): Observable<Warehouse[]> {
-    // Implementation goes here
-    return new Observable<Warehouse[]>();
-  }
+//   getWarehousesBySupplier(supplierId: number): Observable<Warehouse[]> {
+//     return this.http.get<Warehouse[]>(`${this.baseUrl}/warehouse/supplier/${supplierId}`);
+//   }
 
-  addProduct(product: Product): Observable<any> {
-    // Implementation goes here
-    return new Observable<any>();
-  }
+//   addProduct(product: Product): Observable<any> {
+//     return this.http.post<Product>(`${this.baseUrl}/product`, product);
+//   }
 
-  editProduct(product: Product): Observable<any> {
-    // Implementation goes here
-    return new Observable<any>();
-  }
+//   editProduct(product: Product): Observable<any> {
+//     return this.http.put<any>(`${this.baseUrl}/product/${product.productId}`, product);
+//   }
 
-  deleteProduct(productId: number): Observable<any> {
-    // Implementation goes here
-    return new Observable<any>();
-  }
+//   deleteProduct(productId: number): Observable<any> {
+//     return this.http.delete(`${this.baseUrl}/product/${productId}`);
+//   }
 
-  getProductById(productId: number): Observable<Product> {
-    // Implementation goes here
-    return new Observable<Product>();
-  }
+//   getProductById(productId: number): Observable<Product> {
+//     return this.http.get<Product>(`${this.baseUrl}/product/${productId}`);
+//   }
 
-  getAllProducts(): Observable<Product[]> {
-    // Implementation goes here
-    return new Observable<Product[]>();
-  }
+//   getAllProducts(): Observable<Product[]> {
+//     return this.http.get<Product[]>(`${this.baseUrl}/product`);
+//   }
 
-  getAllProductByWarehouse(warehouseId: string | null): Observable<Product[]> {
-    // Implementation goes here
-    return new Observable<Product[]>();
-  }
+//   getAllProductByWarehouse(warehouseId: number | null): Observable<Product[]> {
+//     return this.http.get<Product[]>(`${this.baseUrl}/product/warehouse/${warehouseId}`);
+//   }
 
-}
+// }
